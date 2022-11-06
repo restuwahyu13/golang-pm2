875ac28929e28b930a34d589b4f630bdc4e642bc
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2022-11-06 21:14:19 +0700

    publish files

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const { exec } = require('node:child_process')
+const { promisify } = require('node:util')
 
 /**
  * @description bostraping function for running golang binary process
  */
 
-;(() => {
-	exec('./main', (err, res) => {
-		if (err) console.info(`Go app error ${err}`)
-		console.info(`Go app success: ${res}`)
-	})
-})(
+;(async () => {
+ try {
+   const execPromise = promisify(exec)
+   const res = await execPromise('./main')
+   if (!res.stderr) console.info(`Go app success ${err}`)
+ } catch (e) {
+   console.error(`Go app error: ${res}`)
+   Promise.reject(e)
+ }
+})()
